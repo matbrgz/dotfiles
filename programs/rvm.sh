7781e8ee22a10684a007bcbbc59a984179cf5b19
Matheus Rocha Vieira <matheusrv@email.com>
2018-10-26 14:11:17 -0300

    Update rvm.sh

--- programs/rvm.sh
+++ programs/rvm.sh
@@ -6,6 +6,7 @@ PREVIOUS_PWD="$(jq -r '.pwd' "${HOME}"/tmp/pwd.json)"
 if [ "$(jq -r '.purge' "${PREVIOUS_PWD}"/bootstrap/settings.json)" == y ] ; then
   sudo apt -y purge ruby* rvm*
 fi
+# TODO: Fix 'failed: IPC connect call failed gpg: keyserver receive failed: No dirmngr' on install rvm key.
 sudo apt -y install dirmngr
 gpg-connect-agent reloadagent /bye
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
