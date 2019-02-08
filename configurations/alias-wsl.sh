927cdce46fba12437c066ae478ab37e7cece98b9
Matheus Rocha Vieira <matheusrv@email.com>
2019-02-08 17:29:24 -0200

    Many Improves

--- configurations/alias-wsl.sh
+++ configurations/alias-wsl.sh
@@ -1,1 +1,6 @@
 #!/bin/bash -e
+echo '
+# Alias to run Windows cmd.exe from WSL
+alias cmd="/mnt/c/Windows/System32/cmd.exe"
+alias cmdc="/mnt/c/Windows/System32/cmd.exe /c"
+' >> "${HOME}"/.bash_aliases
