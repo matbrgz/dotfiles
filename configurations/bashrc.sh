88314ebb3f586d644c7cc0a6dbe32562d3f3098e
Matheus Rocha Vieira <matheusrv@email.com>
2019-04-30 15:48:23 -0300

    Project Refresh

--- configurations/bashrc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-PREVIOUS_PWD="$(jq -r '.pwd' "${HOME}"/tmp/pwd.json)"
-defaultfolder="$(jq -r '.defaultfolder' "${PREVIOUS_PWD}"/bootstrap/settings.json)"
-if [ ! -z "${defaultfolder}" ] ; then
-	#Configuring ~/.bashrc
-	echo "cd ${defaultfolder}" >> "${HOME}"/./bashrc
-fi
--- programs/rlang.sh
+++ programs/rlang.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 PREVIOUS_PWD="$(jq -r '.pwd' "${HOME}"/tmp/pwd.json)"
 if [ "$(jq -r '.configurations.debug' "${PREVIOUS_PWD}"/bootstrap/settings.json)" == true ] ; then
-	# Disable exit on non 0
 	set +e
 else
-	# Enable exit on non 0
 	set -e
 fi
 RELEASE_VERSION="$(lsb_release -cs)"
