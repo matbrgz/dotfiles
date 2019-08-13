af68a56cb64bd406d291594d72b2c5e55bde8cbe
Matheus Rocha Vieira <matheusrv@email.com>
2019-08-13 17:09:53 -0300

    Ignore Settings && X11 Server Implement

---
+++ programs/x11server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PREVIOUS_PWD="$1"
+if [ "$(jq -r '.configurations.debug' "${PREVIOUS_PWD}"/bootstrap/settings.json)" == true ]; then
+	set +e
+else
+	set -e
+fi
+sudo apt -y install x11-apps
+{
+	DISPLAY=:0.0
+    export DISPLAY
+} >>"${HOME}"/.bashrc
