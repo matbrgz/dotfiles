d3e7739ed899ed48cefce6bf095ca4d55629b279
Matheus Rocha Vieira <matheusrv@email.com>
2026-04-02 19:58:35 -0300

    feat: implement powershell, shfmt and windows fresh install
    Major expansion for Windows support and script formatting tools.

--- programs/apache-config.sh
+++ programs/apache-config.sh
@@ -12,6 +12,9 @@ fi
 echo " [ DOING ] Apache: Default Dev folder as initial directory on localhost"
 port="$(jq -r '.programs[] | select(.program=="apache").port' "${PREVIOUS_PWD}"/bootstrap/settings.json)"
 defaultfolder="$(jq -r '.personal.defaultfolder' "${PREVIOUS_PWD}"/bootstrap/settings.json)"
+if [ -d /var/www ]; then
+      sudo rm -f -R /var/www
+fi
 sudo rm -f /etc/apache"${APACHE_VERSION}"/sites-available/000-default.conf
 sudo echo "<VirtualHost *:${port}>
      ServerAdmin webmaster@localhost
@@ -27,9 +30,14 @@ sudo echo "<VirtualHost *:${port}>
      ErrorLog ${APACHE_LOG_DIR}/error.log
      CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>" | sudo tee -a /etc/apache"${APACHE_VERSION}"/sites-available/000-default.conf
+if [ ${port} != 80 ]; then
+      sudo sed -i "/Listen 80/c\Listen "${port}"" /etc/apache2/ports.conf
+fi
 sudo /etc/init.d/apache"${APACHE_VERSION}" stop
 echo " [ DOING ] Apache: Allowing mod rewrite rules"
 sudo a2enmod rewrite
 sudo /etc/init.d/apache"${APACHE_VERSION}" start
 echo " [ DOING ] Apache: Allow autoindex for editing apache directory listing"
 sudo a2enmod autoindex
+
+sudo service apache2 restart || sudo service apache2 start
