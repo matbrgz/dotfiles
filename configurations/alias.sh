c023f8a2f1b62a16d5afde6b4914a1d24dc25ab5
Matheus Rocha Vieira <matheusrv@email.com>
2026-04-02 19:58:30 -0300

    chore: apply multiple system improvements
    General performance and reliability enhancements across all scripts.

--- configurations/alias.sh
+++ configurations/alias.sh
@@ -1,1 +1,29 @@
 #!/bin/bash -e
+echo "
+# ls aliases
+alias la=\"ls -al\"
+alias ls=\"ls -h --color --group-directories-first\" # flat view w/ directories first
+alias l=\"ls -h --color --group-directories-first\" # same as above
+alias ll=\"ls -lv --group-directories-first\" # non-flat view
+alias lm=\"ll | more\"
+
+# Folder alias
+alias hdir=\"cd ${HOME}\"
+# shellcheck disable=SC2139
+alias wdir=\"cd ${defaultfolder}\"
+
+# Quick parent-directory aliases
+alias ..=\"cd ..\"
+alias ...=\"cd ../..\"
+alias ....=\"cd ../../..\"
+alias .....=\"cd ../../../..\"
+alias ......=\"cd ../../../../..\"
+alias .......=\"cd ../../../../../..\"
+alias ........=\"cd ../../../../../../..\"
+alias .........=\"cd ../../../../../../../..\"
+
+# Others
+alias editbash=\"nano ${HOME}/.bashrc\"
+alias editba=\"nano ${HOME}/.bash_aliases\"
+alias resource=\"source ${HOME}/.bashrc\"
+" >> "${HOME}"/.bash_aliases
