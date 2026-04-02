f34ba77198b3b0a3a9f9fa9ee77bbcde90b5a2cd
Matheus Breguêz <matbrgz@gmail.com>
2026-04-02 19:58:31 -0300

    chore: fix versioning metadata
    Standardized version tracking files.

--- bootstrap/version.sh
+++ bootstrap/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-if ! curl https://raw.githubusercontent.com/MatheusRV/dotfiles/wsl/bootstrap/version.json --create-dirs -o "${PREVIOUS_PWD}"/bootstrap/version.json
+if ! curl https://raw.githubusercontent.com/MatheusRV/dotfiles/master/bootstrap/version.json --create-dirs -o "${PREVIOUS_PWD}"/bootstrap/version.json
 then
 	echo "Download failed! Exiting."
 	exit 1
