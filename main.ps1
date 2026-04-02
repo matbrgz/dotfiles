37ba6f7403c27ab23d60be2ad1483581df75bbd2
Matheus Breguêz <matbrgz@gmail.com>
2026-04-02 19:58:35 -0300

    feat: expand yarn, pip and windows support
    Enhanced package manager integration for Node and Python.

--- main.ps1
+++ main.ps1
@@ -1,17 +1,12 @@
 Disable-UAC
 
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
-Set-TaskbarOptions -Dock Bottom -Combine Full -Lock
-Set-TaskbarOptions -Dock Bottom -Combine Full -AlwaysShowIconsOn
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
 Programs = @(
     #"sysinternals -y"
     "geforce-game-ready-driver"
 
     "Microsoft-Hyper-V-All -source windowsFeatures"
     "Microsoft-Windows-Subsystem-Linux -source windowsfeatures"
+    "Net-Framework-Core -source windowsFeatures"
     "git"
     "github-desktop"
     "insomnia-rest-api-client"
@@ -52,8 +47,10 @@ Remove-Variable Programs
 #Dark Theme
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
 
-Install-WindowsFeature Net-Framework-Core -source \\network\share\sxs
-Get-WindowsFeature
+#Settings
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+Set-TaskbarOptions -Dock Bottom -Combine Full -Lock
+Set-TaskbarOptions -Dock Bottom -Combine Full -AlwaysShowIconsOn
 
 Enable-UAC
 
@@ -69,6 +66,7 @@ if ($env:computername -ne $ComputerName) {
     Rename-Computer -NewName $ComputerName
 }
 Remove-Variable NameList
-Remove-Variable ComputerName
 
-bash -c "git clone https://github.com/MatheusRV/dotfiles && chmod -R 777 dotfiles && cd dotfiles && ./install.sh"
+bash -c "git clone https://github.com/MatheusRV/dotfiles && chmod -R 777 dotfiles && cd dotfiles && ./install.sh "$ComputerName""
+
+Remove-Variable ComputerName
