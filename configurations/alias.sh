fe121af7e565bb30915b794f5de187031866eb80
Matheus Breguêz <matbrgz@gmail.com>
2026-04-02 19:58:35 -0300

    feat: implement powershell, shfmt and windows fresh install
    Major expansion for Windows support and script formatting tools.

--- configurations/alias.sh
+++ configurations/alias.sh
@@ -23,17 +23,81 @@ alias wdir=\"cd ${defaultfolder}\"
 # Quick parent-directory aliases
 alias ..=\"cd ..\"
 alias ...=\"cd ../..\"
-alias ....=\"cd ../../..\"
-alias .....=\"cd ../../../..\"
-alias ......=\"cd ../../../../..\"
-alias .......=\"cd ../../../../../..\"
-alias ........=\"cd ../../../../../../..\"
-alias .........=\"cd ../../../../../../../..\"
+alias .4=\"cd ../../..\"
+alias .5=\"cd ../../../..\"
+alias .6=\"cd ../../../../..\"
+alias .7=\"cd ../../../../../..\"
+alias .8=\"cd ../../../../../../..\"
+alias .9=\"cd ../../../../../../../..\"
+
+# File and Folder Permission
+alias mx=\"chmod a+x\"
+alias 000=\"chmod 000\"
+alias 644=\"chmod 644\"
+alias 755=\"chmod 755\"
+alias 777=\"chmod 777\"
 
 # Others
 alias editbash=\"nano ${HOME}/.bashrc\"
 alias editba=\"nano ${HOME}/.bash_aliases\"
 alias resource=\"source ${HOME}/.bashrc\"
+alias hosts=\"nano /mnt/c/Windows/System32/drivers/etc/hosts\"
+alias code=\"code .\"
+alias np=\"cmd.exe /c notepad\"
+
+
+# Handy Extract Program
+function extract()      
+{
+    if [ -f \$1 ] ; then
+        case \$1 in
+            *.tar.bz2)   tar xvjf \$1     ;;
+            *.tar.gz)    tar xvzf \$1     ;;
+            *.bz2)       bunzip2 \$1      ;;
+            *.rar)       unrar x \$1      ;;
+            *.gz)        gunzip \$1       ;;
+            *.tar)       tar xvf \$1      ;;
+            *.tbz2)      tar xvjf \$1     ;;
+            *.tgz)       tar xvzf \$1     ;;
+            *.zip)       unzip \$1        ;;
+            *.Z)         uncompress \$1   ;;
+            *.7z)        7z x \$1         ;;
+            *)           echo \"'\$1' cannot be extracted via >extract<\" ;;
+        esac
+    else
+        echo \"'\$1' is not a valid file!\"
+    fi
+}
+ 
+ 
+# Make Dir and CD to it
+function mcd ()
+{
+    mkdir -p \$1
+    cd \$1
+}
+ 
+# Switch a File or Folder
+function swap()
+{
+    if [ ! -z \"\$2\" ] && [ -e \"\$1\" ] && [ -e \"\$2\" ] && ! [ \"\$1\" -ef \"\$2\" ] && (([ -f \"\$1\" ] && [ -f \"\$2\" ]) || ([ -d \"\$1\" ] && [ -d \"\$2\" ])) ; then
+        tmp=$(mktemp -d $(dirname \"\$1\")/XXXXXX)
+        mv \"\$1\" \"\$tmp\" && mv \"\$2\" \"\$1\" &&  mv \"\$tmp\"/\"\$1\" \"\$2\"
+        rmdir \"\$tmp\"
+    else
+        echo \"Usage: swap file1 file2 or swap dir1 dir2\"
+    fi
+}
+ 
+# Creates an archive (*.tar.gz) from given directory.
+function maketar() { tar cvzf \"\${1%%/}.tar.gz\"  \"\${1%%/}/\"; }
+ 
+# Create a ZIP archive of a file or folder.
+function makezip() { zip -r \"\${1%%/}.zip\" \"\$1\" ; }
+ 
+# Make your directories and files access rights sane.
+function sanitize() { chmod -R u=rwX,g=rX,o= \"\$@\" ;}
+
 " >>"${HOME}"/.bash_aliases
 
 if [[ ! "$(uname -r)" =~ "Microsoft$" ]]; then
---
+++ programs/powershell.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PREVIOUS_PWD="${PWD}"
+if [ "$(jq -r '.configurations.debug' "${PREVIOUS_PWD}"/bootstrap/settings.json)" == true ]; then
+	set +e
+else
+	set -e
+fi
+RELEASE_VERSION="$(lsb_release -rs)"
+POWERSHELL_VERSION="$(jq -r '.POWERSHELL_VERSION' "${PREVIOUS_PWD}"/bootstrap/settings.json)"
+if [ "$(jq -r '.configurations.purge' "${PREVIOUS_PWD}"/bootstrap/settings.json)" == y ]; then
+	sudo apt -y purge powershell
+fi
+if ! curl https://github.com/PowerShell/PowerShell/releases/download/v"${POWERSHELL_VERSION}"/powershell_"${POWERSHELL_VERSION}"-1.ubuntu."${RELEASE_VERSION}"_amd64.deb -o powershell_"${POWERSHELL_VERSION}".ubuntu."${RELEASE_VERSION}"_amd64.deb && dpkg -i powershell_"${POWERSHELL_VERSION}".ubuntu."${RELEASE_VERSION}"_amd64.deb; then
+	echo "Powershell Download failed! Skipping."
+	kill $$
+fi
+sudo apt install -yf
